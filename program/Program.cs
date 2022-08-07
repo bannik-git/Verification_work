8a9a730bea10bcefb2ae3a39442c4e7ed74fe75b
Nikolay <nikolay_git@rambler.ru>
2022-08-07 14:34:18 +0500

    Добавлены две функции: запос данных у пользователя и заполнение массива

---
+++ program/Program.cs
@@ -0,0 +1,28 @@
+﻿string GetAnswer (string text)
+{
+    Console.Write(text);
+    return Console.ReadLine() ?? String.Empty;
+}
+
+void FillArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" Введите элемент массива №{i + 1}: ");
+        array[i] = Console.ReadLine() ?? String.Empty;
+    }
+}
+
+ 
+
+
+
+int size = Convert.ToInt32(GetAnswer("Введите длину массива: "));
+string[] array = new string[size];
+
+Console.Clear();
+Console.WriteLine("Заполнение массива:");
+FillArray(array);
+
+
+
