5541af2697a6a57c70bd68c8627ff37fd03fe17c
Nikolay <nikolay_git@rambler.ru>
2022-08-07 15:48:14 +0500

    Итоговая версия программы

--- program/Program.cs
+++ program/Program.cs
@@ -7,22 +7,43 @@
 void FillArray (string[] array)
 {
     for (int i = 0; i < array.Length; i++)
+        array[i] = GetAnswer($" Введите элемент массива №{i + 1}: ");
+}
+
+void FillResultArray (string[] resultArray, string[] initialArray, int desiredLength)
+{
+    int i = 0;
+    for (int j = 0; j < initialArray.Length; j++)
     {
-        Console.Write($" Введите элемент массива №{i + 1}: ");
-        array[i] = Console.ReadLine() ?? String.Empty;
+        if (initialArray[j].Length <= desiredLength)
+        {
+            resultArray[i] = initialArray[j];
+            i++;
+        }
     }
 }
 
- 
-
-
+void PrintArray (string text, string[] array)
+{
+    Console.WriteLine();
+    Console.WriteLine($"{text}[{string.Join(", ", array)}]\n");
+}
 
 int size = Convert.ToInt32(GetAnswer("Введите длину массива: "));
+int desiredStringLength = 3; //Длина строки по условию должна быть меньше или равна 3 символам
 string[] array = new string[size];
 
 Console.Clear();
 Console.WriteLine("Заполнение массива:");
 FillArray(array);
+PrintArray("Ваш массив: ", array);
 
+int sizeResultArray = 0;
 
+for (int i = 0; i < array.Length; i++)
+    if (array[i].Length <= desiredStringLength)
+        sizeResultArray++;
 
+string[] resultArray = new string[sizeResultArray];
+FillResultArray(resultArray, array, desiredStringLength);
+PrintArray("Итоговый массив: ", resultArray);
